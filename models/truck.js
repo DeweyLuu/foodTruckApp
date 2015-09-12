ae90960224bc4fd8843ccf59cada5dc4cf2c1d0d
Dewey Luu <deweyluu@gmail.com>
2015-09-12 15:25:54 -0700

    refreshed routes

--- models/truck.js
+++ models/truck.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var truckSchema = new Schema({
-	name: String,
-	type: Number,
-	location: {
-		longitude: Number,
-		latitude: Number
+	Name: String,
+	Type: Number,
+	Location: {
+		Longitude: Number,
+		Latitude: Number
 	},
-	city: String
+	City: String
 });
 
 module.exports = mongoose.model('Truck', truckSchema);
--- route/truck_router.js
+++ route/truck_router.js
@@ -1,3 +1,4 @@
+var express = require('express');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -14,22 +15,38 @@ module.exports = function(router) {
 				console.log(err);
 			} else {
 				console.log(data);
+				console.log('successfully get');
 				res.json(data);
 			}
 		});
 	})
 
-	router.route('/trucks')
+	router.route('/trucks/find')
 	.post(function(req, res) {
+		//Truck.find({'City': req.body.City}), function(err, data) {
+		Truck.find(findShit(), function(err, data) {
+			if (err) {
+				console.log(err);
+			}
+			console.log(data);
+			res.json(data);
+		})
+	});
+
+	router.route('/trucks/add')
+	.post(function(req, res) {
+		console.log('accessed post route');
 		var newTruck = new Truck(req.body);
 		newTruck.save(function(err, data) {
 			if (err) {
 				console.log(err);
 				return res.status(500).json({result: 1, message: 'server error'});
 			}
+			console.log('added');
 			res.json({result: 0});
 		});
 	});
-
-
+}
+function findShit(req, res, callback) {
+	{'City': req.body.City}
 }
