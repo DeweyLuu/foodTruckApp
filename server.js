d1217bd05f562646afc36a567f9202afff4acc1e
Dewey Luu <deweyluu@gmail.com>
2015-09-15 21:27:59 -0700

    hard coded name and pw

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ process.env.MONGO_URL = 'mongodb://localhost/foodtruck';
 
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/foodtruck');
 */
-
+var theDb = 'mongodb://foodtruck:starbucks99@ds027819.mongolab.com:27819/foodtrucks';
+/*
 var theDb = process.env.MONGO_URL =  'mongodb://' + process.env.MONGOUSER + ':' +
 	process.env.MONGOPW + '@ds027819.mongolab.com:27819/foodtrucks' || process.env.MONGO_URL;
-
+*/
 mongoose.connect(theDb, function(err) {
 	if (err) {
 		console.log(err);
