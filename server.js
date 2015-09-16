4adb3c13ade34184145d322257087b41cdba2e18
Dewey Luu <deweyluu@gmail.com>
2015-09-15 22:06:59 -0700

    hard coded name and pw

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var port = process.env.PORT || 3000;
+//var port = process.env.PORT || 3000;
 
 var truckRoute = express.Router();
 
