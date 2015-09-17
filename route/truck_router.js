38251f10664f64222cb3261789b43bc93b563742
Dewey Luu <deweyluu@gmail.com>
2015-09-17 12:38:14 -0700

    another route for types revised

--- route/truck_router.js
+++ route/truck_router.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var cuisineTypes = require('./cuisineTypes.js');
 
 var bodyParser = require('body-parser');
 var Truck = require('../models/truck.js');
@@ -42,59 +43,8 @@ module.exports = function(router) {
 
 	router.route('/trucks/types')
 	.get(function(req, res) {
-		var results = {};
-		var resultToSend = [];
-		var types = [
-			{type: 'Italian',
-				value: '0x01'},
-			{type: 'Mexican',
-				value: '0x2'},
-			{type: 'Chinese',
-				value: '0x4'},
-			{type: 'Indian',
-				value: '0x8'},
-			{type: 'Thai',
-				value: '0x10'},
-			{type: 'Japanese',
-				value: '0x20'},
-			{type: 'Greek',
-				value: '0x40'},
-			{type: 'American',
-				value: '0x80'},
-  		{type: 'Spanish',
-  			value: '0x100'},
-  		{type: 'French',
-  			value: '0x200'},
-  		{type: 'Mediterranean',
-  			value: '0x400'},
-  		{type: 'Lebanese',
-  			value: '0x800'},
-  		{type: 'Vietnamese',
-  			value: '0x1000'},
-  		{type: 'Korean',
-  			value: '0x2000'},
-  		{type: 'Turkish',
-  			value: '0x4000'},
-  		{type: 'Moroccan',
-  			value: '0x8000'},
-  		{type: 'Soul',
-  			value: '0x10000'},
-  		{type: 'Caribbean',
-  			value: '0x20000'},
-  		{type: 'German',
-  			value: '0x40000'}
-		];
-
-		for (var cuisine in types) {
-			results[cuisine] = types[cuisine];
-			resultToSend.push();
-		}
-		var typeToSend = JSON.stringify(types);
-		console.log(resultToSend);
-		console.log(JSON.stringify(types));
-		console.log(types);
-		//res.send(JSON.stringify(results));
-		res.send(JSON.stringify(types));
+		console.log(JSON.stringify(cuisineTypes));
+		res.send(JSON.stringify(cuisineTypes));
 	})
 
 	router.route('/trucks/:name', function(req, res) {
